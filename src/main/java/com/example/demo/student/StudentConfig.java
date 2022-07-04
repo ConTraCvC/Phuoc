b579fd2a59704d42120d57bda9c4998412821d97
ConTraCvC <cdn11phuoc@gmail.com>
2022-07-04 23:39:15 +0700

    Ok Guy

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -4,31 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 @Configuration
 public class StudentConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
-                    Student van = new Student(
-                            "Van",
-                            "van@phuoc",
-                            LocalDate.of(2000, Month.APRIL, 5)
-                    );
-
-                    Student phuoc = new Student(
-                            "Phuoc",
-                            "phuoc@van",
-                        LocalDate.of(1999, Month.JANUARY, 16)
-
-                    );
-                    repository.saveAll(
-                            List.of(van, phuoc)
-                    );
         };
     }
 }
