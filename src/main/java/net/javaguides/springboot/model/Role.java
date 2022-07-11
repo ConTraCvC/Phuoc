6193b3348ad9de55b0170354b9aab575bc95ee2f
ConTraCvC <cdn11phuoc@gmail.com>
2022-07-11 07:20:57 +0700

    OK OK

---
+++ src/main/java/net/javaguides/springboot/model/Role.java
@@ -0,0 +1,42 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+	@Id
+	@SequenceGenerator(
+			name = "role_sequence",
+			sequenceName = "role_sequence",
+			allocationSize = 1
+
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "role_sequence")
+	private Long id;
+	private String name;
+	
+	public Role() {
+		
+	}
+	
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
