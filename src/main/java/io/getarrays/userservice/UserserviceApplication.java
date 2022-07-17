f17101f4677faf70bf5b9e081f9bada380a1d997
ConTraCvC <cdn11phuoc@gmail.com>
2022-07-17 13:14:45 +0700

    OK COOL

---
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -0,0 +1,38 @@
+package io.getarrays.userservice;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "USER"));
+			userService.saveRole(new Role(null, "ADMIN"));
+
+			userService.saveUser(new User(null, "Phuoc", "phuoc", "123", new ArrayList<>()));
+
+			userService.addRole("phuoc", "ADMIN");
+		};
+	}
+}
