4d711fc0ae21bcce93aaf392c3c3fd3106ea6637
Phuoc <106910649+ConTraCvC@users.noreply.github.com>
2023-01-03 16:18:12 +0700

    yes

--- src/main/java/com/jwt/controllers/ModController.java
+++ src/main/java/com/jwt/controllers/ModController.java
@@ -6,8 +6,6 @@ import com.jwt.repository.RefreshTokenRepository;
 import com.jwt.repository.UserRepository;
 import com.jwt.security.services.PasswordResetImpl;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
