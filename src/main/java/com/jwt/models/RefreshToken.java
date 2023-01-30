d12d6cd36d93e8b9aefae832ee462aa6bab74bf1
Phuoc <106910649+ConTraCvC@users.noreply.github.com>
2023-01-30 09:06:22 +0700

    yes
    (cherry picked from commit b32a4173fca58e18d43834c64632a82f3909d47d)
    
    # Conflicts:
    #	src/main/java/com/jwt/controllers/AuthController.java

--- src/main/java/com/jwt/models/RefreshToken.java
+++ src/main/java/com/jwt/models/RefreshToken.java
@@ -13,7 +13,7 @@ public class RefreshToken {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @OneToOne
+  @OneToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "user_id", referencedColumnName = "id")
   private User user;
 
