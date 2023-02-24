bf6f2984310a2a3106a4d88758fb35ae5d3c5a5b
Phuoc <106910649+ConTraCvC@users.noreply.github.com>
2023-02-24 17:36:03 +0700

    oki

--- src/main/java/com/jwt/security/services/PasswordResetImpl.java
+++ src/main/java/com/jwt/security/services/PasswordResetImpl.java
@@ -162,7 +162,7 @@ public class PasswordResetImpl implements PasswordReset{
 
   @Override
   public ResponseEntity<?> resetPasswordOTP(ChangePasswordRequest password, Otp otpCodex) {
-    Twilio.init("AC428df5bd302a88e1e314d9ece0159181", "d60b5c6548496920f5d27bb9d2220bac");
+    Twilio.init("AC428df5bd302a88e1e314d9ece0159181", "f85809bb54f256fee337feaa79a8c4b1");
     Optional<User> user = userRepository.findByEmail(password.getEmail());
     if(user.isPresent()) {
       Optional<Otp> otp = otpRepository.findByUserId(user.get().getId());
@@ -178,13 +178,13 @@ public class PasswordResetImpl implements PasswordReset{
         return ResponseEntity.badRequest().body("Set OtpToken failed");
       }
     }
-//    try {
-//      Message.creator(new PhoneNumber("+84866682422"),
-//              new PhoneNumber("+19497495157"),
-//              "Limited reset OTP code for 10 minutes: " + otpCodex.getOtp()).create();
-//    } catch (Exception e) {
-//      return ResponseEntity.badRequest().body("Send SMS failed");
-//    }
+    try {
+      Message.creator(new PhoneNumber("+84866682422"),
+              new PhoneNumber("+19497495157"),
+              "Limited reset OTP code for 10 minutes: " ).create();
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body("Send SMS failed");
+    }
     return ResponseEntity.badRequest().body("Wrong email address");
   }
 
